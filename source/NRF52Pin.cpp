d2a541aedd67f7564ab8204db32700ac6713f98b
hensonm1 <m.henson1@lancaster.ac.uk>
2026-05-20 09:50:19 +0100

    override istouched

--- source/NRF52Pin.cpp
+++ source/NRF52Pin.cpp
@@ -584,6 +584,7 @@ int NRF52Pin::isAnalog()
  */
 int NRF52Pin::isTouched()
 {
+    return 1;
     // Maintain the last type of sensing used.
     return isTouched(status & IO_STATUS_CAPACITATIVE_TOUCH ? TouchMode::Capacitative : TouchMode::Resistive);
 }
